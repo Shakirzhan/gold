c165f85ea7d34ac08f8232d7eb4c64d2de6ba25f
Shakirzhan Samarbaev <samarbaev99@mail.ru>
2018-05-22 17:53:47 +0300

    added the main files

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp'),
+	concatCss = require('gulp-concat-css'),
+	browserSync = require('browser-sync'),
+    server = require('browser-sync'),
+    less = require('gulp-less'),
+    csso = require('gulp-csso'),
+    watchLess = require('gulp-watch-less'),
+    plumber = require('gulp-plumber');
+
+gulp.task('style', function() 
+{
+    return gulp.src('less/style.less')
+//      .pipe(watchLess('/less/style.less'))
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(gulp.dest('css/'))
+        .pipe(server.reload({stream: true}));
+});
+
+gulp.task('serve', ['style'], function() 
+{
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch('less/**/*.less', ['style']);
+    gulp.watch('*.html')
+        .on('change', browserSync.reload);
+});
+
+//gulp.task('default', function() 
+//{
+//	return gulp.src('/**/*.*')
+//	   .pipe(gulp.dest('dist/html'));
+//});
+
+//gulp.task('default', function () 
+//{
+//    return gulp.src('/less/style.less')
+//        .pipe(watchLess('/less/style.less'))
+//        .pipe(less())
+//        .pipe(gulp.dest('/css/'));
+//});
+
+gulp.task('concat', function () {
+  return gulp.src('css/*.css')
+    .pipe(concatCss("css/main.css"))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('browser-sync', function() 
+{
+    browserSync.init({
+        server: {
+            baseDir: "/"
+        }
+    });
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+//gulp.task('less', function() 
+//{
+//    return gulp.src("app/less/*.less") // находим все less файлы в папке less 
+//        .pipe(less()) // собственно компилируем их
+//        .pipe(csso()) // если нужно - сжимаем css код (если не нужно, строчку можно удалить)
+//        .pipe(concatCss('main.css')) // при желании можно объединить все в один css-файл 
+//        .pipe(gulp.dest("app/css")) // выгружаем файлы в папку app в раздел css 
+//        .pipe(browserSync.stream()); // при желании можно обновить browser-sync после изменений
+//});
